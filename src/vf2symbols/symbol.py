f79d8e2b6bf03c77771939ecf85697fbcc509e4e
EsoHasInsomnia <iiahmed@google.com>
2021-01-12 00:35:12 -0800

    add unittests for write_symbol_from_svg_test.py reformat py files.

--- src/vf2symbols/symbol.py
+++ src/vf2symbols/symbol.py
@@ -11,17 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Represents an Apple custom symbol."""
 import os
 
 from typing import Any
 
-from fontTools.misc.transform import Transform
 from fontTools.pens.svgPathPen import SVGPathPen
 from fontTools.pens.transformPen import TransformPen
 from lxml import etree  # pytype: disable=import-error
-from nanoemoji import color_glyph
 from picosvg.geometric_types import Rect
 from picosvg.svg import SVG
 from picosvg.svg_transform import Affine2D
@@ -29,46 +26,48 @@ from picosvg.svg_transform import Affine2D
 _SYMBOL_SIZE = 120
 # This value was chosen by a designer to center the SVG inside the template.
 _SYMBOL_DY_MULTIPLE = -0.7942
+_SYMBOL_FILEPATH = os.path.join(os.path.dirname(__file__), "symbol_template.svg")
 
 
-class Symbol():
+class Symbol:
+    def __init__(self, symbol_filepath=_SYMBOL_FILEPATH):
+        self.symbol = SVG.parse(symbol_filepath)
 
-    def __init__(self):
-        self.symbol = SVG.parse(os.path.join(os.path.dirname(__file__), "symbol_template.svg"))
-
-    
-    def write_icon(self, symbol_wght_name: str, drawable_path: Any, svg_pen: SVGPathPen, rect: Rect) -> None:
+    def write_icon(
+        self, symbol_wght_name: str, drawable_path: Any, svg_pen: SVGPathPen, rect: Rect
+    ) -> None:
         """Writes a drawable object to the current symbol.
 
         Args:
-          symbol_wght_name: Symbol variant name to place the symbol at (e.g. Regular-M).
-          drawable_path: Any drawable object that supports the FontTools pen protocol (e.g.
-          SVGPath).
+          symbol_wght_name: Symbol variant name to place the symbol at (e.g.
+            Regular-M).
+          drawable_path: Any drawable object that supports the FontTools pen
+            protocol (e.g. SVGPath).
           svg_pen: SVGPathPen for drawing the path.
-          rect: The bounding box co-ordinates of the given svg_path used to position the drawable object
-          correctly in the symbol space, for example, in the icon fonts where the Y-axis is flipped, 
-          the height is expected to be -ve.
+          rect: The bounding box co-ordinates of the given svg_path used to
+            position the drawable object correctly in the symbol space, for
+            example, in the icon fonts where the Y-axis is flipped, the height
+            is expected to be -ve.
         """
         parent = self.symbol.xpath_one(f'//svg:g[@id="{symbol_wght_name}"]')
         path = etree.SubElement(parent, "path")
-        path.attrib["d"] = self._draw_svg_path(drawable_path, svg_pen, self._build_transformation(rect))
-        
-        
+        path.attrib["d"] = self._draw_svg_path(
+            drawable_path, svg_pen, self._build_transformation(rect)
+        )
+
     def _draw_svg_path(self, svg_path, svg_pen, transform):
         svg_path.draw(TransformPen(svg_pen, transform))
-        return " ".join(svg_pen._commands) 
-       
-    def _build_transformation(self, rect):
-        return Affine2D.rect_to_rect(rect, Rect(0, 0, _SYMBOL_SIZE, _SYMBOL_SIZE)).translate(0, _SYMBOL_DY_MULTIPLE * rect.h)
+        return " ".join(svg_pen._commands)
 
-        
-              
+    def _build_transformation(self, rect):
+        return Affine2D.rect_to_rect(
+            rect, Rect(0, 0, _SYMBOL_SIZE, _SYMBOL_SIZE)
+        ).translate(0, _SYMBOL_DY_MULTIPLE * rect.h)
+
     def drop_empty_icons(self):
         for empty_icon in self.symbol.xpath(f"//svg:g[not(*)]"):
             empty_icon.getparent().remove(empty_icon)
-    
-    
+
     def write_to(self, filename):
         with open(filename, "w") as f:
             f.write(self.symbol.tostring())
-
--- src/vf2symbols/write_symbol_from_svg.py
+++ src/vf2symbols/write_symbol_from_svg.py
@@ -11,19 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Generates an Apple custom symbol using an SVG by placing it in the required variant."""
-import os
 import re
+import sys
 
 from absl import app
 from absl import flags
-from absl import logging
 
 from fontTools import svgLib
 from fontTools.pens.svgPathPen import SVGPathPen
-from picosvg.svg import SVG
 from picosvg.geometric_types import Rect
+from picosvg.svg import SVG
 from vf2symbols.symbol import Symbol
 
 _REQUIRED_SYMBOL = "Regular-M"
@@ -36,7 +34,7 @@ flags.DEFINE_string("out", None, "Output file.")
 
 def parse_float(string):
     return float(re.compile("\d+([.]\d*)?").match(string).group(0))
-    
+
 
 def main(argv):
     if len(argv) > 2:
@@ -44,10 +42,20 @@ def main(argv):
     symbol = Symbol()
     pico = SVG.parse(argv[1]).topicosvg()
     main_svg = pico.xpath_one("//svg:svg")
-    symbol.write_icon(_REQUIRED_SYMBOL, svgLib.SVGPath.fromstring(pico.tostring()), SVGPathPen(None), Rect(0, 0, parse_float(main_svg.get("width")), parse_float(main_svg.get("height"))))
+    symbol.write_icon(
+        _REQUIRED_SYMBOL,
+        svgLib.SVGPath.fromstring(pico.tostring()),
+        SVGPathPen(None),
+        Rect(
+            0,
+            0,
+            parse_float(main_svg.get("width")),
+            parse_float(main_svg.get("height")),
+        ),
+    )
     symbol.drop_empty_icons()
     symbol.write_to(FLAGS.out)
-    
+
 
 if __name__ == "__main__":
     app.run(main)
